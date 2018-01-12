ec2b8656f26cec8a84d2aee9b41e4d5c19004cd9
Shivam Soni <shivam@studiosubu.com>
2018-01-12 21:50:19 +0530

    Round 3 commits

--- ali-block/custom.js
+++ ali-block/custom.js
@@ -1,4 +1,27 @@
-
+$(".animsition").animsition({
+    inClass: 'fade-in-up'
+    , outClass: 'fade-out-up'
+    , inDuration: 1500
+    , outDuration: 1000
+    , linkElement: '.animsition-link'
+    , // e.g. linkElement: 'a:not([target="_blank"]):not([href^="#"])'
+    loading: true
+    , loadingParentElement: 'body', //animsition wrapper element
+    loadingClass: 'animsition-loading'
+    , loadingInner: '', // e.g '<img src="loading.svg" />'
+    timeout: false
+    , timeoutCountdown: 5000
+    , onLoadEvent: true
+    , browser: ['animation-duration', '-webkit-animation-duration']
+    , // "browser" option allows you to disable the "animsition" in case the css property in the array is not supported by your browser.
+    // The default setting is to disable the "animsition" in a browser that does not support "animation-duration".
+    overlay: false
+    , overlayClass: 'animsition-overlay-slide'
+    , overlayParentElement: 'body'
+    , transition: function (url) {
+        window.location.href = url;
+    }
+});
 
 $(document).ready(function () {
     
@@ -44,6 +67,9 @@ $(document).ready(function () {
         }
     }
     scaleimg();
+    $('body').on('animsition.inEnd', function(){
+        scaleimg();
+    });
     $(window).on('resize', function(){
         scaleimg();
     });
