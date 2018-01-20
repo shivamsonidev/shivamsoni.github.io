a0cfd826c1684712ea25c06cf8278867e7f99e02
Shivam Soni <shivam@studiosubu.com>
2018-01-20 19:43:25 +0530

    Launch edits

--- ali-block/custom.js
+++ ali-block/custom.js
@@ -6,8 +6,8 @@ $(".animsition").animsition({
     , linkElement: '.animsition-link'
     , // e.g. linkElement: 'a:not([target="_blank"]):not([href^="#"])'
     loading: true
-    , loadingParentElement: 'body', //animsition wrapper element
-    loadingClass: 'animsition-loading'
+    , loadingParentElement: 'body' //animsition wrapper element
+    , loadingClass: 'animsition-loading'
     , loadingInner: '', // e.g '<img src="loading.svg" />'
     timeout: false
     , timeoutCountdown: 5000
